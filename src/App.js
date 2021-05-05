6dc7273506826949c1b0c7c435af04c2026e0486
Francesco Stiffoni <fstiffo@sgajo.com>
2021-05-05 13:31:00 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./styles.css";
+import { Button } from "@material-ui/core";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <Button variant="contained" color="primary">
+        Hello World
+      </Button>
+    </div>
+  );
+}
