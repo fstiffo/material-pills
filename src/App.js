fc749dff20e28cd4144915dde407f1381a593142
Francesco Stiffoni <fstiffo@sgajo.com>
2021-05-08 20:18:53 +0200

    Add purchases table

--- src/App.js
+++ src/App.js
@@ -3,15 +3,16 @@ import { Button } from "@material-ui/core";
 import { Dexie } from "dexie";
 import { DataGrid, GridToolbar } from "@material-ui/data-grid";
 import { useDexie, useDexieTable } from "use-dexie";
+import dayjs from "dayjs";
 
 export default function App() {
   useDexie(
     "PILLS_DB",
     {
       medications:
-        "++id,&[brandName+strength+unity], name, manufacturer, quantity",
+        "++id,&[brandName+strength+unity+quantity], name, manufacturer",
       prescriptions: "++id, &[drugName+dose+unity], days",
-      
+      purchases: "++id, medicationId, quantity, date"
     },
     (db) => {
       db.medications.count().then((count) => {
@@ -42,11 +43,22 @@ export default function App() {
         { drugName: "aspirina", dose: 100, unity: "mg", days: 1 },
         { drugName: "paracetamolo", dose: 500, unity: "mg", days: 7 }
       ]);
+
+      db.purchases.clear();
+      db.purchases.count().then((count) => {
+        if (count === 0) {
+          db.purchases.bulkPut([
+            { medicationId: 1, quantity: 1, date: dayjs().format() },
+            { medicationId: 2, quantity: 3, date: dayjs().format() }
+          ]);
+        }
+      });
     }
   );
 
   const medications = useDexieTable("medications") || [];
   const prescriptions = useDexieTable("prescriptions") || [];
+  const purchases = useDexieTable("purchases") || [];
 
   const medsCols = [
     { field: "brandName", headerName: "Brand Name", width: 160 },
@@ -81,6 +93,17 @@ export default function App() {
         }`
     }
   ];
+  const pursCols = [
+    {
+      field: "purchaseDay",
+      headerName: "Date",
+      valueGetter: (params) =>
+        `${dayjs(params.getValue("date")).format("DD/MM/YYYY")}`,
+width: 120
+    },
+    { field: "medicationId", headerName: "Medication", width: 130 },
+    { field: "quantity", headerName: "Qty", width: 80 }
+  ];
 
   return (
     <div className="App">
@@ -94,9 +117,13 @@ export default function App() {
         <h2>Medications</h2>
         <DataGrid rows={medications} columns={medsCols} pageSize={5} />
       </div>
-      <div style={{ height: 400, width: "100%" }}>
+      <div style={{ height: 400, width: "100%", marginBottom: 80 }}>
         <h2>Prescriptions</h2>
         <DataGrid rows={prescriptions} columns={presCols} pageSize={5} />
+      </div>
+      <div style={{ height: 400, width: "100%", marginBottom: 80 }}>
+        <h2>Purchases</h2>
+        <DataGrid rows={purchases} columns={pursCols} pageSize={5} />
       </div>
     </div>
   );
